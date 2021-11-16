73ec567a47a48b30f7e3d4a311e66e66b69f0905
rguibi <rguibi.arjdal@gmail.com>
2021-11-17 00:31:49 +0100

    line order modif

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,8 +7,8 @@ class SaleOrderLine(models.Model):
     
     order_ref = fields.Char('Order Reference',related='order_id.name')   
     customer_id = fields.Many2one('res.partner',related='order_id.partner_id')
-    date_order = fields.Date('Date',related='order_id.date_order')
-    commitment_date = fields.Date('Date livraison',related='order_id.commitment_date')
+    date_order = fields.Datetime('Date',related='order_id.date_order')
+    commitment_date = fields.Datetime('Date livraison',related='order_id.commitment_date')
     partner_shipping_id = fields.Many2one('res.partner',related='order_id.partner_shipping_id')
 
 
